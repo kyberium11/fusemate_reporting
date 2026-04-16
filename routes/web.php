2273a8fa7e8285df9f4902a12881fa3a3d5c2c48
kyberium11 <kyberium11@gmail.com>
2026-04-16 23:36:54 +0800

    Add /clear-cache endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Services\ReportingService;
 
 Route::get('/', function () {
@@ -11,4 +12,17 @@ Route::get('/generate-report', function (ReportingService $reportingService) {
     $result = $reportingService->run();
     
     return response()->json($result);
+});
+
+Route::get('/clear-cache', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('view:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('cache:clear');
+    
+    return response()->json([
+        'status' => 'success',
+        'message' => 'All caches cleared successfully.'
+    ]);
 });
